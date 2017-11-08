688849186b54c024e699fbe9c4cc209eab03fb98
jamesharrell71 <james.harrell71@gmail.com>
2017-11-08 15:40:55 -0600

    Created http search service for use in retrieving from the college database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SchoolSearchComponent } from './school-search/school-search.component';
-import {Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { SelectionPageComponent } from './selection-page/selection-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ClassViewPageComponent } from './class-view-page/class-view-page.component';
 
+import { SchoolSearchService} from './school-search/school-search.service';
+import { HttpModule } from '@angular/http';
 const appRoutes: Routes = [
-    { path: '', redirectTo: 'search', pathMatch: 'full'},
+    { path: '', redirectTo: '#/search', pathMatch: 'full'},
     {
-        path: 'search',
+        path: '#/search',
         component: SelectionPageComponent,
         data: { title: 'Home Page' }
     },
     {
-        path: 'classes',
+        path: '#/classes',
         component: ClassViewPageComponent,
         data: { title: 'Classes View' }
     },
-    { path: '**', component: PageNotFoundComponent }
+    { path: '#/**', component: PageNotFoundComponent }
 ];
 @NgModule({
   declarations: [
@@ -32,9 +34,10 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+      HttpModule,
       RouterModule.forRoot(appRoutes, {useHash: true})
   ],
-  providers: [],
+  providers: [SchoolSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
