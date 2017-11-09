13ed712d678806cd1f2d3c943ca6efe27fbadf46
James Harrell <james.harrell71@gmail.com>
2017-11-09 03:08:51 -0600

    Created services to read from a REST api to retrieve college data. Also implemented search results properly linking to the table displaying relevant information.

--- src/app/school-search/school-search.component.ts
+++ src/app/school-search/school-search.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { SchoolSearchService} from './school-search.service';
+import {Component, OnInit} from '@angular/core';
+import {SchoolSearchService} from './school-search.service';
 
 declare var $: any;
+
 @Component({
-  selector: 'app-school-search',
-  templateUrl: './school-search.component.html',
-  styleUrls: ['./school-search.component.css']
+    selector: 'app-school-search',
+    templateUrl: './school-search.component.html',
+    styleUrls: ['./school-search.component.css']
 })
 export class SchoolSearchComponent implements OnInit {
 
-  constructor(private searchService: SchoolSearchService) {
-  }
-  colleges = [];
-  ngOnInit() {
-      this.searchService.getColleges().subscribe(responseCol => this.colleges = responseCol);
-      $('#search-select').dropdown();
-      $('#state-select').dropdown();
+    constructor(private searchService: SchoolSearchService) {
 
-  }
+    }
 
+    colleges = [];
+    collegeSelection;
+
+    ngOnInit() {
+
+        this.searchService.getColleges().subscribe(
+            responseCol => {
+                this.colleges = responseCol;
+                this.handleData();
+            });
+
+    }
+
+    handleData() {
+        $('.ui.search').search({
+            source: this.colleges,
+            searchFields: [
+                'title',
+                'location'
+            ]
+
+        });
+    }
+
+    onSubmit() {
+        this.collegeSelection = $('.ui.search').search('get result');
+    }
 }
